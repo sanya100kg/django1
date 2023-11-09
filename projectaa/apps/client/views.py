0d9e7be2dc5873abbea3f55e1c32f8db13bc6cd2
sanya100kg <68840680+sanya100kg@users.noreply.github.com>
2023-11-09 22:12:33 +0700

    test
    test

---
+++ projectaa/apps/client/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import ClientSerializer
+from .models import Client
+from django.core.exceptions import PermissionDenied
+
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class=ClientSerializer
+    quaryset=Client.objects.all()
+
+    def get_queryset(self):
+        return self.quaryset.filter(created_by=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+    def perform_update(self, serializer):
+        obj=self.get_object()
+
+        if self.request.user!=obj.created_by:
+            raise PermissionDenied('Wrong object owner')
+        
+        serializer.save()
